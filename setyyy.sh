61e3d9acffd2927bcea28478422fbd845c5e9a31
hansiu <unchallenged.yo@gmail.com>
2017-01-22 20:52:10 +0100

    usuniete niepotrzebne

--- setyyy.sh
+++
@@ -1,7 +0,0 @@
-python setup.py build
-python setup.py install
-python3 setup.py build
-python3 setup.py install
-python testybiovcf.py
-echo "przerwaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-python3 testybiovcf.py
